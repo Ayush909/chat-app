46f7e4dd3c99e6ef7f5f79df1e4da4070ec1472a
Ayush909 <kushwahaayu9@gmail.com>
2020-04-18 00:50:32 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const socket = io('http://localhost:3000');
+const messageForm = document.getElementById('send-container');
+const messageContainer = document.getElementById('message-container');
+const usercontainer = document.getElementById('user-count');
+const messageInput = document.getElementById('message-input');
+const userName = document.getElementById('user-name');
+
+const name = prompt('What is your name?');
+appendMessage('You joined');
+socket.emit('new-user',name);
+
+socket.on('chat-message', data=>{
+    appendMessage(`${data.name} : ${data.msg}`);
+})
+
+socket.on('user-connected', name=>{
+    appendMessage(`${name} connected`);
+})
+
+socket.on('account-details',(data)=>{
+    userName.innerText = 'Your name: '+data.name;
+})
+
+socket.on('user-count',(data)=>{
+    userdetails(`No of Users connected : ${data.no_of_users}`);
+})
+
+socket.on('user-disconnected' , name=>{
+    appendMessage(`${name} disconnected`)
+})
+
+function userdetails(message){
+    usercontainer.innerText = message;
+}
+
+messageForm.addEventListener('submit', e=>{
+    e.preventDefault();
+    const message = messageInput.value;
+    appendMessage(`You: ${message}`);
+    socket.emit('send-chat-message',message);
+    messageInput.value = '';
+})
+
+function appendMessage(message){
+    const msgElement = document.createElement('div');
+    msgElement.innerText = message;
+    messageContainer.append(msgElement);
+}
---
+++ server.js
@@ -0,0 +1,23 @@
+const io = require('socket.io')(3000);
+const users = {};
+let no_of_users = 0;
+io.on('connection', socket =>{
+    no_of_users++;
+    socket.broadcast.emit('user-count',{ no_of_users : no_of_users});
+    socket.emit('user-count',{no_of_users: no_of_users});
+
+    socket.on('new-user',name=>{
+        users[socket.id] = name;
+        socket.emit('account-details',{ name: users[socket.id] } );
+        socket.broadcast.emit('user-connected',name);
+    })
+    socket.on('send-chat-message', msg =>{
+        socket.broadcast.emit('chat-message',{ msg: msg , name : users[socket.id] });
+    })
+    socket.on('disconnect', ()=>{
+        no_of_users--;
+        socket.broadcast.emit('user-count',{ no_of_users : no_of_users});
+        socket.broadcast.emit('user-disconnected', users[socket.id]);
+        delete users[socket.id];
+    })
+})
