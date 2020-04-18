a106a1919c3329728d86336291f24b33da10d1e5
dizzle2003 <ajaliedennis@yahoo.com>
2020-04-18 09:39:26 +0100

    added an express server with environment variables and added a stylesheet

--- script.js
+++ script.js
@@ -7,42 +7,42 @@ const userName = document.getElementById('user-name');
 
 const name = prompt('What is your name?');
 appendMessage('You joined');
-socket.emit('new-user',name);
+socket.emit('new-user', name);
 
-socket.on('chat-message', data=>{
-    appendMessage(`${data.name} : ${data.msg}`);
-})
+socket.on('chat-message', (data) => {
+  appendMessage(`${data.name} : ${data.msg}`);
+});
 
-socket.on('user-connected', name=>{
-    appendMessage(`${name} connected`);
-})
+socket.on('user-connected', (name) => {
+  appendMessage(`${name} connected`);
+});
 
-socket.on('account-details',(data)=>{
-    userName.innerText = 'Your name: '+data.name;
-})
+socket.on('account-details', (data) => {
+  userName.innerText = 'Your name: ' + data.name;
+});
 
-socket.on('user-count',(data)=>{
-    userdetails(`No of Users connected : ${data.no_of_users}`);
-})
+socket.on('user-count', (data) => {
+  userdetails(`No of Users connected : ${data.no_of_users}`);
+});
 
-socket.on('user-disconnected' , name=>{
-    appendMessage(`${name} disconnected`)
-})
+socket.on('user-disconnected', (name) => {
+  appendMessage(`${name} disconnected`);
+});
 
-function userdetails(message){
-    usercontainer.innerText = message;
+function userdetails(message) {
+  usercontainer.innerText = message;
 }
 
-messageForm.addEventListener('submit', e=>{
-    e.preventDefault();
-    const message = messageInput.value;
-    appendMessage(`You: ${message}`);
-    socket.emit('send-chat-message',message);
-    messageInput.value = '';
-})
+messageForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const message = messageInput.value;
+  appendMessage(`You: ${message}`);
+  socket.emit('send-chat-message', message);
+  messageInput.value = '';
+});
 
-function appendMessage(message){
-    const msgElement = document.createElement('div');
-    msgElement.innerText = message;
-    messageContainer.append(msgElement);
+function appendMessage(message) {
+  const msgElement = document.createElement('div');
+  msgElement.innerText = message;
+  messageContainer.append(msgElement);
 }
--- server.js
+++ server.js
@@ -1,23 +1,26 @@
-const io = require('socket.io')(3000);
+const express = require('express')();
+const PORT = process.env.PORT || 3000;
+const server = express.listen(PORT);
+const io = require('socket.io')(server);
 const users = {};
 let no_of_users = 0;
-io.on('connection', socket =>{
-    no_of_users++;
-    socket.broadcast.emit('user-count',{ no_of_users : no_of_users});
-    socket.emit('user-count',{no_of_users: no_of_users});
+io.on('connection', (socket) => {
+  no_of_users++;
+  socket.broadcast.emit('user-count', { no_of_users: no_of_users });
+  socket.emit('user-count', { no_of_users: no_of_users });
 
-    socket.on('new-user',name=>{
-        users[socket.id] = name;
-        socket.emit('account-details',{ name: users[socket.id] } );
-        socket.broadcast.emit('user-connected',name);
-    })
-    socket.on('send-chat-message', msg =>{
-        socket.broadcast.emit('chat-message',{ msg: msg , name : users[socket.id] });
-    })
-    socket.on('disconnect', ()=>{
-        no_of_users--;
-        socket.broadcast.emit('user-count',{ no_of_users : no_of_users});
-        socket.broadcast.emit('user-disconnected', users[socket.id]);
-        delete users[socket.id];
-    })
-})
+  socket.on('new-user', (name) => {
+    users[socket.id] = name;
+    socket.emit('account-details', { name: users[socket.id] });
+    socket.broadcast.emit('user-connected', name);
+  });
+  socket.on('send-chat-message', (msg) => {
+    socket.broadcast.emit('chat-message', { msg: msg, name: users[socket.id] });
+  });
+  socket.on('disconnect', () => {
+    no_of_users--;
+    socket.broadcast.emit('user-count', { no_of_users: no_of_users });
+    socket.broadcast.emit('user-disconnected', users[socket.id]);
+    delete users[socket.id];
+  });
+});
